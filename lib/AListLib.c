0a4875633f03068808f996327f2dc0f1265ea5ea
Duc Huy (Hui) <48280785+huyluongme@users.noreply.github.com>
2021-10-11 20:01:39 +0700

    Update AListLib.c

--- lib/AListLib.c
+++ lib/AListLib.c
@@ -1,108 +1,79 @@
 #include <stdio.h>
 
-#define Maxlength 20
+#define MaxLenght 100
+
 typedef int ElementType;
 typedef int Position;
 
 typedef struct {
-	ElementType Elements [Maxlength];
-	int Last;
+	ElementType Elements[MaxLenght];
+	Position Last;
 }List;
 
-void makenullList(List *pL)
-{
-	(*pL).Last=0;
+void makenullList(List* pL){
+	pL->Last = 0;
 }
 
-int fullList(List L)
-{
-	return L.Last==Maxlength;
+int emptyList(List L){
+	return L.Last == 0;
 }
 
-int emptyList(List L)
-{
-	return L.Last==0;
+int fullList(List L){
+	return L.Last == MaxLenght;
 }
 
-Position first (List L)
-{
+Position first(List L){
 	return 1;
 }
 
-Position endList(List L){
-    return L.Last+1;
+Position endList(List L) {
+	return L.Last + 1;
 }
 
-Position next(Position P, List L){
-    return P+1;
+Position next(Position P, List L) {
+	return P + 1;
 }
 
-Position previous(Position P, List L){
-    return P-1;
+Position previous(Position P, List L) {
+	return P - 1;
 }
 
-ElementType retrieve(Position P, List L){
-    return L.Elements[P-1];
+ElementType retrieve(Position P, List L) {
+	return L.Elements[P - 1];
 }
 
-void insertList(ElementType x, Position p, List *pL)
-{
+void insertList(ElementType X, Position P, List* pL) {
 	if (fullList(*pL))
-		printf("Danh sach day");	
-  	else if ((p<1) || (p>pL->Last+1))
-	    printf("Vi tri khong hop le");
-	else 
-	{
-	  Position q;   
-	  for(q=pL->Last; q>=p; q--)
-		 pL->Elements[q]=pL->Elements[q-1];	  
-	  pL->Elements[p-1]=x;	  
-	  pL->Last++;
-	 }
-}
-
-void deleteList(Position P, List *L){
-    if((P > L->Last) || (P<1))
-	printf("\nVi tri khong hop le !!!");
-    else
-	if(emptyList(*L))
-	  printf("\nDanh sach rong !");
-	else{
-	  Position i;
-	  for(i=P; i<L->Last; i++)
-	  {
-	     L->Elements[i-1] = L-> Elements[i];
-	  }
-	  L->Last--;
+		printf("Danh sach day");
+	else if (P < 1 || P > endList(*pL))
+		printf("Vi tri khong hop le");
+	else {
+		for (Position i = endList(*pL) - 1; i >= P; i--)
+			pL->Elements[i] = pL->Elements[i - 1];
+		pL->Elements[P - 1] = X;
+		pL->Last++;
 	}
 }
 
-Position locate (ElementType x, List L)
-{
-	Position p=first(L);
-	int f=0;
-	while (p!=endList(L)&& f==0)
-	{
-		if (retrieve (p,L)==x)
-			f=1;
-		else 
-			p=next(p,L)	;
-	}
-	return p;
-}
-
-void readList(List* pL) {
-	makenullList(pL);
-	scanf("%d", &pL->Last);
-	for (int i = 1; i <= pL->Last; i++)
-	{
-		scanf("%d", &pL->Elements[i - 1]);
+void deleteList(Position P, List* pL) {
+	if (emptyList(*pL))
+		printf("Danh sach rong");
+	else if (P < 1 || P > endList(*pL))
+		printf("Vi tri khong hop le");
+	else {
+		for (Position i = P; i < endList(*pL) - 1; i++)
+			pL->Elements[i - 1] = pL->Elements[i];
+		pL->Last--;
 	}
 }
 
-void printList(List L) {
-	for (int i = 1; i <= L.Last; i++)
-	{
-		printf("%d ", L.Elements[i - 1]);
+Position locate(ElementType X, List L) {
+	Position P = first(L);
+	Position E = endList(L);
+	while (P != E) {
+		if (retrieve(P, L) == X)
+			return P;
+		else
+			P = next(P, L);
 	}
 }
